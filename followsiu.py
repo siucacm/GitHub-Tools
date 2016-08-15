edbeac32a94f4b9f4c5a026766e84121e1360fd9
Ethan Richardson <ethanx94@gmail.com>
2016-08-15 14:53:54 -0500

    Update and rename followuw.py to followsiu.py

--- followsiu.py
+++ followsiu.py
@@ -1,9 +1,9 @@
 # FollowUW
 # Usage:
-#       1. git clone git@github.com:csu/GitHub-Tools.git
+#       1. git clone git@github.com:siucacm/GitHub-Tools.git
 #       2. cd GitHub-Tools
 #       3. pip install -r requirements.txt
-#       4. python followuw.py
+#       4. python followsiu.py
 
 import re
 import urllib2
@@ -15,7 +15,7 @@ pw = getpass.getpass()
 
 gh = Github(login=username, password=pw)
 
-text = urllib2.urlopen('https://raw.githubusercontent.com/udubacm/students/master/README.md').read()
+text = urllib2.urlopen('https://raw.githubusercontent.com/siucacm/students/master/README.md').read()
 people = re.findall('http[s]?://github.com/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 for person in people:
     person = person[:-1] # because I suck at regex
